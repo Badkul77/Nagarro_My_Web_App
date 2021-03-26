262288b22089681cc1e89515365426434f53b003
Naman Badkul <naman.badkul@nagarro.com>
2021-03-26 13:30:54 +0530

    docker file deleted

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM openjdk:nanoserver
-EXPOSE 8084
-ADD target/our-web-app.war our-web-app.war
-ENTRYPOINT ["java","-war","/our-web-app.war"]
--- src/main/test/AppTest.java
+++ src/main/test/AppTest.java
@@ -7,28 +7,26 @@ import org.junit.Test;
 import com.nagarro.ourapp.controller.AdditionController;
 
 public class AppTest {
-	 AdditionController app = new AdditionController();
-
-	    @Test
-	    public void testApp()
-	    {
-	        assertEquals("Hello world", app.greet("world"));
-	    }
-
-	    @Test
-	    public void testAppData()
-	    {
-	        assertNotNull(app);
-	    }
-	    @Test
-	    public void testTrue()
-	    {
-	        assertTrue(app.isValid());
-	    	
-	    }
-	    @Test
-	    public void testAgeCalc()
-	    {
-	   assertEquals("You are 21 Year 2 months and 22 days Old",app.findAge(22, 3, 2021, 31, 12, 1999));
-	    }
+AdditionController app=new AdditionController();
+@Test
+public void testApp()
+{
+    assertEquals("Hello world", app.greet("world"));
+}
+@Test
+public void testAppData()
+{
+    assertNotNull(app);
+}
+@Test
+public void testTrue()
+{
+    assertTrue(app.isValid());
+	
+}
+@Test
+public void testAgeCalc()
+{
+assertEquals("You are 21 Year 2 months and 22 days Old",app.findAge(22, 3, 2021, 31, 12, 1999));
+}
 }
